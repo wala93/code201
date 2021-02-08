b19095d3bc87e224e1f5091db7980eaafbaf363e
Walaa Alomari <walaahmad1987@gmail.com>
2021-02-08 16:36:43 +0200

    initial commit for new repo

---
+++ js/app.js
@@ -0,0 +1,56 @@
+'use strict' ;
+let userName = prompt ('whats your name please ' + ' name');
+alert ('welcom in my guessing game ' + userName );
+
+let myName = prompt( 'my name is Walaa ,do you agree thats true ?' + 'y/n');
+if (myName === 'y' || myName ==='yes')
+ {
+   // console.log ( myName)
+    alert ('true answer ,good ');
+}
+else {
+     alert( 'its wrong you dont know me !') }; 
+
+let myMajor = prompt ('Am I  a civil engineer  '+ 'y/n');
+if (myMajor === 'n' || myMajor === 'no' )
+{
+   // console.log (myMajor)
+alert( 'good answer ');
+}
+else { alert('its wrong you dont know me !')};
+
+let mySituation =prompt ( 'Am I single ?'+ 'y/n');
+
+if (mySituation === 'no' || mySituation === 'n' )
+{
+   // console.log (mySituatio)
+alert( 'good answer ');
+}
+else { alert('its wrong you dont know me !')};
+
+let campus = prompt ('do I prefer to be in campus?' + 'y/n');
+
+if (campus === 'no' || campus === 'n' )
+{
+    // console.log (campus)
+alert( 'good answer ');
+}
+else { alert('its wrong you dont know me !')};
+
+let delecious = prompt ('do I like shawerma ?' + 'y/n');
+
+if (delecious === 'y' || mySituation === 'yes' )
+{
+   // console.log (delecious)
+alert( 'good answer ');
+}
+else { alert('its wrong you dont know me !')};
+
+alert ('thank you alot ' + userName);
+
+
+
+    
+
+
+
